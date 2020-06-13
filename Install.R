376979866f2dbefe9e3ef6e6742168568cf98205
Liying Wang <jaybin502@gmail.com>
2020-06-12 23:54:14 -0700

    create Install.R

---
+++ Install.R
@@ -0,0 +1,19 @@
+# Run the code below: 
+install.packages(c("rnaturalearth", 
+                   "rnaturalearthdata", 
+                   "ggplot2", 
+                   "tidyverse", 
+                   "sf", 
+                   "sp",
+                   "shadowtext", 
+                   "ggmap", 
+                   "ggspatial", 
+                   "raster", 
+                   "spatstat", 
+                   "maptools"))
+
+# If you messages: "Do you want to install from sources the package which needs compilation?" 
+# Type "No" on your console
+
+# Run the code below
+devtools::install_github('3wen/legendMap')
